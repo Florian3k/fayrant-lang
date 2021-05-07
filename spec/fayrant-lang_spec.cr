7b6eb2eb531d805351ecb7382f9f1bc5807f6111
Florian3k <24961583+Florian3k@users.noreply.github.com>
2021-05-07 13:29:30 +0200

    generate project

---
+++ spec/fayrant-lang_spec.cr
@@ -0,0 +1,5 @@
+require "./spec_helper"
+
+describe FayrantLang do
+  # TODO: Write tests
+end
---
+++ src/fayrant-lang.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Fayrant::Lang`
+module FayrantLang
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
