2dcf348b3695f19917d1941b7b02fb597c2f98f4
Florian3k <24961583+Florian3k@users.noreply.github.com>
2021-05-08 22:41:13 +0200

    add basic main code

--- src/fayrant-lang.cr
+++ src/fayrant-lang.cr
@@ -1,6 +1,11 @@
-# TODO: Write documentation for `Fayrant::Lang`
-module FayrantLang
-  VERSION = "0.1.0"
 
-  # TODO: Put your code here
+case ARGV.size
+when 0
+  puts "Running REPL..."
+when 1
+  puts "Running interpreter on [#{ARGV[0]}]"
+else
+  puts "Error: Too many arguents"
+  puts "  Use: fayrant - to run interactive repl"
+  puts "  Use: fayrant <file.fy> - to run file.fy"
 end
