01ec7af63002678475e48004f8c58b16840a929d
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-05-23 16:37:25 +0200

    added abstract expression class

---
+++ src/expression.cr
@@ -0,0 +1,13 @@
+require "../value.cr"
+
+module FayrantLang
+  module AST
+    abstract class Expr
+      abstract def eval(env : Object) : AnyValue # TODO
+
+      def ==(other)
+        false
+      end
+    end
+  end
+end
