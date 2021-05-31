587365f5620cff8a3159e4903ea78f36cefb9426
Florian3k <24961583+Florian3k@users.noreply.github.com>
2021-05-31 09:41:09 +0200

    fix formatting

--- src/ast/expression.cr
+++ src/ast/expression.cr
@@ -11,229 +11,228 @@ module FayrantLang
     end
 
     abstract class LiteralExpr(T) < Expr
-        getter value
+      getter value
 
-        def initialize(@value : T)
-        end
-  
-        def ==(other : LiteralExpr(T))
-          value == other.value
-        end
+      def initialize(@value : T)
+      end
+
+      def ==(other : LiteralExpr(T))
+        value == other.value
+      end
     end
 
     class BooleanLiteralExpr < LiteralExpr(Bool)
-        def initialize(value : Bool)
-          super
-        end
-  
-        def eval(env) : BooleanValue
-          BooleanValue.new @value
-        end
+      def initialize(value : Bool)
+        super
       end
 
-      class NumberLiteralExpr < LiteralExpr(Float64)
-        def initialize(value : Float64)
-          super
-        end
-  
-        def eval(env) : NumberValue
-          NumberValue.new @value
-        end
+      def eval(env) : BooleanValue
+        BooleanValue.new @value
+      end
+    end
+
+    class NumberLiteralExpr < LiteralExpr(Float64)
+      def initialize(value : Float64)
+        super
       end
 
-      class NullLiteralExpr < LiteralExpr(Nil)
-        def initialize(value : Nil = nil)
-          super
-        end
-  
-        def eval(env) : NullValue
-          NullValue.new
-        end
+      def eval(env) : NumberValue
+        NumberValue.new @value
+      end
+    end
+
+    class NullLiteralExpr < LiteralExpr(Nil)
+      def initialize(value : Nil = nil)
+        super
       end
 
-
-      # TODO : Implement string literals 
-
-      abstract class UnaryExpr < Expr
-        getter expr
-  
-        def initialize(@expr : Expr)
-        end
+      def eval(env) : NullValue
+        NullValue.new
       end
-  
-      class UnaryExprMinus < UnaryExpr
-        def initialize(expr : Expr)
-          super
-        end
-  
-        def eval(env) : NumberValue
-          NumberValue.new -@expr.eval(env).getNumber
-        end
-  
-        def ==(other : UnaryExprMinus)
-          expr == other.expr
-        end
+    end
+
+    # TODO : Implement string literals
+
+    abstract class UnaryExpr < Expr
+      getter expr
+
+      def initialize(@expr : Expr)
+      end
+    end
+
+    class UnaryExprMinus < UnaryExpr
+      def initialize(expr : Expr)
+        super
       end
 
-      class UnaryExprNegation < UnaryExpr
-        def initialize(expr : Expr)
-          super
-        end
-  
-        def eval(env) : Boolean
-          BooleanValue.new !@expr.eval(env).getBoolean
-        end
-  
-        def ==(other : UnaryExprNegation)
-          expr == other.expr
-        end
+      def eval(env) : NumberValue
+        NumberValue.new -@expr.eval(env).getNumber
       end
 
-      class UnaryExprToString < UnaryExpr
-        def initialize(expr : Expr)
-          super
-        end
-  
-        def eval(env) : AnyValue
-          StringValue.new @expr.eval(env).toString
-        end
-  
-        def ==(other : UnaryExprToString)
-          expr == other.expr
-        end
+      def ==(other : UnaryExprMinus)
+        expr == other.expr
+      end
+    end
+
+    class UnaryExprNegation < UnaryExpr
+      def initialize(expr : Expr)
+        super
       end
 
-      class UnaryExprToNumber < UnaryExpr
-        def initialize(expr : Expr)
-          super
-        end
+      def eval(env) : Boolean
+        BooleanValue.new !@expr.eval(env).getBoolean
+      end
 
-        def eval(env) : NumberValue
-          case @expr.eval(env).type
-          when ValueType::Number
-            NumberValue.new @expr.eval(env).getNumber
-          when ValueType::Boolean
-            NumberValue.new @expr.eval(env).getBoolean ? 1 : 0
-          when ValueType::String
-            val = @expr.eval(env).getString.to_f64?
-            unless val == Nil
-              @expr.eval(env).getString.to_f64
-            else
-              raise Exception
-            end
+      def ==(other : UnaryExprNegation)
+        expr == other.expr
+      end
+    end
+
+    class UnaryExprToString < UnaryExpr
+      def initialize(expr : Expr)
+        super
+      end
+
+      def eval(env) : AnyValue
+        StringValue.new @expr.eval(env).toString
+      end
+
+      def ==(other : UnaryExprToString)
+        expr == other.expr
+      end
+    end
+
+    class UnaryExprToNumber < UnaryExpr
+      def initialize(expr : Expr)
+        super
+      end
+
+      def eval(env) : NumberValue
+        case @expr.eval(env).type
+        when ValueType::Number
+          NumberValue.new @expr.eval(env).getNumber
+        when ValueType::Boolean
+          NumberValue.new @expr.eval(env).getBoolean ? 1 : 0
+        when ValueType::String
+          val = @expr.eval(env).getString.to_f64?
+          unless val == Nil
+            @expr.eval(env).getString.to_f64
           else
             raise Exception
           end
-        end
-
-        def ==(other : UnaryExprToString)
-          expr == other.expr
+        else
+          raise Exception
         end
       end
 
-      abstract class BinaryExpr < Expr
-        getter lhs
-        getter rhs
-  
-        def initialize(@lhs : Expr, @rhs : Expr)
-        end
+      def ==(other : UnaryExprToString)
+        expr == other.expr
       end
-  
-      class BinaryExprPlus < BinaryExpr
-        def initialize(lhs : Expr, rhs : Expr)
-          super
-        end
-  
-        def eval(env) : NumberValue
-          NumberValue.new @lhs.eval(env).getNumber + @rhs.eval(env).getNumber
-        end
-  
-        def ==(other : BinaryExprPlus)
-          lhs == other.lhs && rhs == other.rhs
+    end
+
+    abstract class BinaryExpr < Expr
+      getter lhs
+      getter rhs
+
+      def initialize(@lhs : Expr, @rhs : Expr)
+      end
+    end
+
+    class BinaryExprPlus < BinaryExpr
+      def initialize(lhs : Expr, rhs : Expr)
+        super
+      end
+
+      def eval(env) : NumberValue
+        NumberValue.new @lhs.eval(env).getNumber + @rhs.eval(env).getNumber
+      end
+
+      def ==(other : BinaryExprPlus)
+        lhs == other.lhs && rhs == other.rhs
+      end
+    end
+
+    class BinaryExprMinus < BinaryExpr
+      def initialize(lhs : Expr, rhs : Expr)
+        super
+      end
+
+      def eval(env) : NumberValue
+        NumberValue.new @lhs.eval(env).getNumber - @rhs.eval(env).getNumber
+      end
+
+      def ==(other : BinaryExprPlus)
+        lhs == other.lhs && rhs == other.rhs
+      end
+    end
+
+    class BinaryExprMult < BinaryExpr
+      def initialize(lhs : Expr, rhs : Expr)
+        super
+      end
+
+      def eval(env) : NumberValue
+        NumberValue.new @lhs.eval(env).getNumber * @rhs.eval(env).getNumber
+      end
+
+      def ==(other : BinaryExprPlus)
+        lhs == other.lhs && rhs == other.rhs
+      end
+    end
+
+    class BinaryExprDiv < BinaryExpr
+      def initialize(lhs : Expr, rhs : Expr)
+        super
+      end
+
+      def eval(env) : NumberValue
+        unless @rhs.eval(env).getNumber == 0
+          NumberValue.new @lhs.eval(env).getNumber / @rhs.eval(env).getNumber
+        else
+          raise Exception
         end
       end
 
-      class BinaryExprMinus < BinaryExpr
-        def initialize(lhs : Expr, rhs : Expr)
-          super
-        end
-  
-        def eval(env) : NumberValue
-          NumberValue.new @lhs.eval(env).getNumber - @rhs.eval(env).getNumber
-        end
-  
-        def ==(other : BinaryExprPlus)
-          lhs == other.lhs && rhs == other.rhs
+      def ==(other : BinaryExprPlus)
+        lhs == other.lhs && rhs == other.rhs
+      end
+    end
+
+    class BinaryExprDivInv < BinaryExpr
+      def initialize(lhs : Expr, rhs : Expr)
+        super
+      end
+
+      def eval(env) : NumberValue
+        unless @lhs.eval(env).getNumber == 0
+          NumberValue.new @rhs.eval(env).getNumber / @lhs.eval(env).getNumber
+        else
+          raise Exception
         end
       end
 
-      class BinaryExprMult < BinaryExpr
-        def initialize(lhs : Expr, rhs : Expr)
-          super
-        end
-  
-        def eval(env) : NumberValue
-          NumberValue.new @lhs.eval(env).getNumber * @rhs.eval(env).getNumber
-        end
-  
-        def ==(other : BinaryExprPlus)
-          lhs == other.lhs && rhs == other.rhs
+      def ==(other : BinaryExprPlus)
+        lhs == other.lhs && rhs == other.rhs
+      end
+    end
+
+    class BinaryExprMod < BinaryExpr
+      def initialize(lhs : Expr, rhs : Expr)
+        super
+      end
+
+      def eval(env) : NumberValue
+        unless @rhs.eval(env).getNumber == 0
+          NumberValue.new @lhs.eval(env).getNumber % @rhs.eval(env).getNumber
+        else
+          raise Exception
         end
       end
 
-      class BinaryExprDiv < BinaryExpr
-        def initialize(lhs : Expr, rhs : Expr)
-          super
-        end
-  
-        def eval(env) : NumberValue
-          unless @rhs.eval(env).getNumber == 0
-            NumberValue.new @lhs.eval(env).getNumber / @rhs.eval(env).getNumber
-          else
-            raise Exception
-          end
-        end
-  
-        def ==(other : BinaryExprPlus)
-          lhs == other.lhs && rhs == other.rhs
-        end
+      def ==(other : BinaryExprPlus)
+        lhs == other.lhs && rhs == other.rhs
       end
-
-      class BinaryExprDivInv < BinaryExpr
-        def initialize(lhs : Expr, rhs : Expr)
-          super
-        end
-  
-        def eval(env) : NumberValue
-          unless @lhs.eval(env).getNumber == 0
-            NumberValue.new @rhs.eval(env).getNumber / @lhs.eval(env).getNumber
-          else
-            raise Exception
-          end
-        end
-  
-        def ==(other : BinaryExprPlus)
-          lhs == other.lhs && rhs == other.rhs
-        end
-      end
-
-      class BinaryExprMod < BinaryExpr
-        def initialize(lhs : Expr, rhs : Expr)
-          super
-        end
-  
-        def eval(env) : NumberValue
-          unless @rhs.eval(env).getNumber == 0
-            NumberValue.new @lhs.eval(env).getNumber % @rhs.eval(env).getNumber 
-          else
-            raise Exception
-          end
-        end
-  
-        def ==(other : BinaryExprPlus)
-          lhs == other.lhs && rhs == other.rhs
-        end
-      end
+    end
 
     class BinaryExprExpt < BinaryExpr
       def initialize(lhs : Expr, rhs : Expr)
@@ -376,6 +375,5 @@ module FayrantLang
     end
 
     # TODO: VALIDATE THE CODE AND WRITE TESTS!
-
   end
 end
