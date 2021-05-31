3418781ac9e3146025bb0d14aa2f3f7f4e4ba942
Florian3k <24961583+Florian3k@users.noreply.github.com>
2021-05-31 16:56:24 +0200

    cleanup literal tests

--- spec/ast/expression_spec.cr
+++ spec/ast/expression_spec.cr
@@ -16,17 +16,17 @@ describe "FayrantLang::AST Expressions" do
         ex1.should eq ex1
         ex1.should eq ex2
         ex2.should_not eq ex3
-        (ex3 == ex4).should eq true
-        (ex4 == ex4).should eq true
+        ex3.should eq ex4
+        ex4.should eq ex4
       end
 
       it "BooleanLiteralExpr should evaluate to BooleanValue" do
         ex1 = BooleanLiteralExpr.new true
         ex2 = BooleanLiteralExpr.new false
 
-        (ex1.eval(Nil) == BooleanValue.new true).should eq true
-        (ex1.eval(Nil) == BooleanValue.new false).should eq false
-        (ex2.eval(Nil) == BooleanValue.new false).should eq true
+        ex1.eval(Nil).should eq BooleanValue.new true
+        ex1.eval(Nil).should_not eq BooleanValue.new false
+        ex2.eval(Nil).should eq BooleanValue.new false
       end
     end
 
@@ -37,35 +37,35 @@ describe "FayrantLang::AST Expressions" do
         ex3 = NumberLiteralExpr.new 7
         ex4 = NumberLiteralExpr.new 7
 
-        (ex1 == ex1).should eq true
-        (ex1 == ex2).should eq true
-        (ex2 == ex3).should eq false
-        (ex3 == ex4).should eq true
-        (ex4 == ex4).should eq true
+        ex1.should eq ex1
+        ex1.should eq ex2
+        ex2.should_not eq ex3
+        ex3.should eq ex4
+        ex4.should eq ex4
       end
 
       it "NumberLiteralExpr should evaluate to NumberValue" do
         ex1 = NumberLiteralExpr.new 3
         ex2 = NumberLiteralExpr.new 7
 
-        (ex1.eval(Nil) == NumberValue.new 3).should eq true
-        (ex1.eval(Nil) == NumberValue.new 7).should eq false
-        (ex2.eval(Nil) == NumberValue.new 7).should eq true
+        ex1.eval(Nil).should eq NumberValue.new 3
+        ex1.eval(Nil).should_not eq NumberValue.new 7
+        ex2.eval(Nil).should eq NumberValue.new 7
       end
     end
 
     describe "NullLiteralExpr" do
-        it "two NullLiteralExpr should be equal" do
-          ex1 = NullLiteralExpr.new
-          ex2 = NullLiteralExpr.new
-  
-          ex1.should eq ex2
-        end
-  
-        it "NullLiteralExpr should evaluate to NullValue" do
-          ex = NullLiteralExpr.new
-          ex.eval(Nil).should eq NullValue.new
-        end
+      it "two NullLiteralExpr should be equal" do
+        ex1 = NullLiteralExpr.new
+        ex2 = NullLiteralExpr.new
+
+        ex1.should eq ex2
       end
+
+      it "NullLiteralExpr should evaluate to NullValue" do
+        ex = NullLiteralExpr.new
+        ex.eval(Nil).should eq NullValue.new
+      end
+    end
   end
 end
